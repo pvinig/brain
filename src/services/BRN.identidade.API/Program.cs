c003bf40e2adc7447a412409be5f77251f752028
pvinig <pvinig@gmail.com>
2023-02-09 19:40:33 -0300

    bug swagger fixed

--- src/services/BRN.identidade.API/Program.cs
+++ src/services/BRN.identidade.API/Program.cs
@@ -24,6 +24,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 
 builder.Services.AddDbContext<ApplicationDbContext>(optionsAction: options =>
         options.UseSqlServer(builder.Configuration.GetConnectionString(name: "DefaultConnection")));
@@ -33,19 +35,29 @@ builder.Services.AddDefaultIdentity<IdentityUser>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultTokenProviders();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline. 
+
 
 var app = builder.Build();
+
+if(app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
 
 app.UseDeveloperExceptionPage();
 
 app.UseHttpsRedirection();
 
+app.UseRouting();
+
 app.UseAuthentication();
 
-app.UseAuthorization();
 
-app.UseRouting();
+app.UseAuthorization();
 
 app.UseEndpoints(endpoints =>
 {
